eef1895e18aae5332160e75409098d43849cffd4
Timothy Sharkey <Techne3@gmail.com>
2020-05-20 12:21:04 -0600

    created main directory

---
+++ Main/build/precache-manifest.35a630e1bc8f19ed56e635d54d903ebe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e32ef5bc53b679d502e86222552ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0f0a38f1bf1a71ec0e3",
+    "url": "/static/css/main.19981cd3.chunk.css"
+  },
+  {
+    "revision": "6500351346b5f5e5897e",
+    "url": "/static/js/2.3dfbfdc0.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.3dfbfdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f0a38f1bf1a71ec0e3",
+    "url": "/static/js/main.32cf883d.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "08f9a451849a94558b1901855a7b63cb",
+    "url": "/static/media/Boy With Glasses.08f9a451.svg"
+  },
+  {
+    "revision": "a17bca9655eca2dcb802f299f356e7d7",
+    "url": "/static/media/WellDone.a17bca96.png"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
